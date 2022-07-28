a5d54871579fbb53f2a96eb031c4ea4dea47c05a
weerawit32 <vasthabo@hotmail.com>
2022-07-28 20:41:56 +0700

    add platform detail and toggle function
    add platform detail and toggle function

--- src/components/platformDetail.js
+++ src/components/platformDetail.js
@@ -1,27 +1,45 @@
-import "../css/component/platformDetail.style.css"
+import "../css/component/platformDetail.style.css";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-const PlatformDetail = ({togglePlatform}) => {
-    return (
-        <div className="platform__conatainer  bg-white text-start">
-            <Row>
-                <Col>
-                <div className="platform__header py-3 border-bottom " onClick={() => togglePlatform()}><h4>Platform</h4></div>
-                    <p className="topic my-5 text-muted fs-4">ADDITIONAL RESOURCES</p>
-                    <ul className="platform__list text-start">
-                        <li className="platform__item">Elastic Clound</li>
-                        <li className="platform__item">View pricing</li>
-                        <li className="platform__item">Free trial and downloads</li>
-                        <li className="platform__item">View all integrations</li>
-
-
-                    </ul>
-                </Col>
-            </Row>
-            
-        </div>
-    );
-}
+const PlatformDetail = ({ togglePlatform }) => {
+  return (
+    <div className="platform__conatainer  bg-white text-start">
+      <Row>
+        <Col>
+          <div
+            className="platform__header py-3 border-bottom "
+            onClick={() => togglePlatform()}
+          >
+            <h4>Platform</h4>
+          </div>
+          <p className="topic my-4 text-muted fs-4">ADDITIONAL RESOURCES</p>
+          <ul className="platform__list text-start">
+            <li className="platform__item">
+              <a href="/" className="platform__link">
+                Elastic Clound
+              </a>
+            </li>
+            <li className="platform__item">
+              <a href="/" className="platform__link">
+                View pricing
+              </a>
+            </li>
+            <li className="platform__item">
+              <a href="/" className="platform__link">
+                Free trial and downloads
+              </a>
+            </li>
+            <li className="platform__item">
+              <a href="/" className="platform__link">
+                View all integrations
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default PlatformDetail;
